1123dff499c049abf4bb44665672fb03bd1cdef5
Jamie Roche <G00411347@ATU.ie>
2023-12-16 19:46:14 +0000

    22

--- dbconfig.py
+++ dbconfig.py
@@ -1,6 +1,6 @@
 mysql = {
     'host':"rochejamie.mysql.pythonanywhere-services.com",
     'user':"rochejamie",
-    'password':"Pa$$ypass123",
+    'password':"Jr200195",
     'database':"rochejamie$DataRep"
 }
