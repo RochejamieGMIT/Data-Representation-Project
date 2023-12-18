5f6eed7503b621739a3b94cfc0a7dc56e49d5054
Jamie Roche <G00411347@ATU.ie>
2023-12-18 20:23:05 +0000

    remove some conditions on server

--- server1.py
+++ server1.py
@@ -67,14 +67,9 @@ def update(id):
     reqJson = request.json
     if 'Land_area' in reqJson and type(reqJson['Land_area']) is not int:
         abort(400)
-    if 'Life_ex' in reqJson and type(reqJson['Life_ex']) is not float:
-        abort(400)
     if 'Population' in reqJson and type(reqJson['Population']) is not int:
         abort(400)
-    if 'Lat' in reqJson and type(reqJson['Lat']) is not float:
-        abort(400)
-    if 'lon' in reqJson and type(reqJson['lon']) is not float:
-        abort(400)
+  
 
     if 'country' in reqJson:
         foundCountrys['country'] = reqJson['country']
