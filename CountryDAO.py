46b27c51534d8d75d9f8b8073ecace7023b2d04d
Jamie Roche <G00411347@ATU.ie>
2023-12-20 21:07:15 +0000

    dddddddddddd

--- CountryDAO.py
+++ CountryDAO.py
@@ -79,7 +79,7 @@ class CountryDAO:
         #sql="select c.country,c.capital,r.region,r.literacy,r.phone from country c inner join regionInfo r on c.country = r.country where r.region = 'WESTERN EUROPE' limit 5"
         #values = (limit,)
         cursor = self.getcursor()
-        sql="select * from regioninfo"
+        sql="select c.country,c.capital,r.region,r.literacy,r.phone from country c inner join regionInfo r on c.country = r.country where r.region = \'WESTERN EUROPE\' limit 5"
         cursor.execute(sql)
         results = cursor.fetchall()
         returnArray = []
